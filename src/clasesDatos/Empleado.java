e0ee53ce6a3df88e2b11e787cc2bb7452a6d9967
Marcos <marcos.movilla05@somo.eus>
2024-12-11 09:48:31 +0100

    Mi primer commit

---
+++ src/clasesDatos/Empleado.java
@@ -0,0 +1,11 @@
+package clasesDatos;
+
+public class Empleado {
+	String dni;
+	String nombre;
+	String apellido;
+	int edad;
+	boolean clubSocial; //Indica si el empleado está en el club social de la empresa
+	float sueldo;
+	
+}
