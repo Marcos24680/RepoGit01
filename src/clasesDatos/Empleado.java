0d5297ea693d49d34959713ec7110010921c3881
Marcos <marcos.movilla05@somo.eus>
2024-12-17 11:49:16 +0100

    Añadidos GETers y SETers

--- src/clasesDatos/Empleado.java
+++ src/clasesDatos/Empleado.java
@@ -57,6 +57,113 @@ public class Empleado {
 		this.clubSocial = false;
 		this.sueldo = (float) 0.0;
 	}
+
+	/**
+	 * GETers and SETers
+	 */
+	public String getDni() {
+		return dni;
+	}
+
+	/**
+	 * @param dni the dni to set
+	 */
+	public void setDni(String dni) {
+		this.dni = dni;
+	}
+
+	/**
+	 * @return the nombre
+	 */
+	public String getNombre() {
+		return nombre;
+	}
+
+	/**
+	 * @param nombre the nombre to set
+	 */
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	/**
+	 * @return the apellido
+	 */
+	public String getApellido() {
+		return apellido;
+	}
+
+	/**
+	 * @param apellido the apellido to set
+	 */
+	public void setApellido(String apellido) {
+		this.apellido = apellido;
+	}
+
+	/**
+	 * @return the categoria
+	 */
+	public String getCategoria() {
+		return categoria;
+	}
+
+	/**
+	 * @param categoria the categoria to set
+	 */
+	public void setCategoria(String categoria) {
+		this.categoria = categoria;
+	}
+
+	/**
+	 * @return the edad
+	 */
+	public int getEdad() {
+		return edad;
+	}
+
+	/**
+	 * @param edad the edad to set
+	 */
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	/**
+	 * @return the clubSocial
+	 */
+	public boolean isClubSocial() {
+		return clubSocial;
+	}
+
+	/**
+	 * @param clubSocial the clubSocial to set
+	 */
+	public void setClubSocial(boolean clubSocial) {
+		this.clubSocial = clubSocial;
+	}
+
+	/**
+	 * @return the sueldo
+	 */
+	public float getSueldo() {
+		return sueldo;
+	}
+
+	/**
+	 * @param sueldo the sueldo to set
+	 */
+	public void setSueldo(float sueldo) {
+		this.sueldo = sueldo;
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
 	
 	
 }
