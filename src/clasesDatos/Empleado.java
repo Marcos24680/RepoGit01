7a68bee779607e58b429ba79303c0e14d0851653
Marcos <marcos.movilla05@somo.eus>
2024-12-17 11:43:53 +0100

    Añadido constructores

--- src/clasesDatos/Empleado.java
+++ src/clasesDatos/Empleado.java
@@ -5,8 +5,58 @@ public class Empleado {
 	private String nombre;
 	private String apellido;
 	private String categoria;
-	int edad;
-	boolean clubSocial; //Indica si el empleado está en el club social de la empresa
-	float sueldo;
+	private int edad;
+	private boolean clubSocial; //Indica si el empleado está en el club social de la empresa
+	private float sueldo;
+	/**
+	 * @param dni
+	 * @param nombre
+	 * @param apellido
+	 * @param categoria
+	 * @param edad
+	 * @param clubSocial
+	 * @param sueldo
+	 */
+	public Empleado(String dni, String nombre, String apellido, String categoria, int edad, boolean clubSocial,
+			float sueldo) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.categoria = categoria;
+		this.edad = edad;
+		this.clubSocial = clubSocial;
+		this.sueldo = sueldo;
+	}
+
+	// Constructor vacio
+	public Empleado() {
+		super();
+		this.dni = "";
+		this.nombre = "***********";
+		this.apellido = "*******";
+		this.categoria = "Base";
+		this.edad = 0;
+		this.clubSocial = false;
+		this.sueldo = (float) 0.0;
+	}
+
+	// Constructor  
+	/**
+	 * @param dni
+	 * @param nombre
+	 * @param apellido
+	 */
+	public Empleado(String dni, String nombre, String apellido) {
+		super();
+		this.dni = dni;
+		this.nombre = nombre;
+		this.apellido = apellido;
+		this.categoria = "Base";
+		this.edad = 0;
+		this.clubSocial = false;
+		this.sueldo = (float) 0.0;
+	}
+	
 	
 }
